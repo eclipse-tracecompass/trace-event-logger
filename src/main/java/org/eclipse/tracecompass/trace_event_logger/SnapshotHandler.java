f8d89e4ad814d810256ec1db01701a496f13e8ea
Matthew Khouzam <matthew.khouzam@ericsson.com>
2024-01-24 16:23:03 -0500

    fix snapshot handler and test it
    Works with events using parameters rather than dedicated getters.
    
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

--- src/main/java/org/eclipse/tracecompass/trace_event_logger/SnapshotHandler.java
+++ src/main/java/org/eclipse/tracecompass/trace_event_logger/SnapshotHandler.java
@@ -37,6 +37,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.LogRecord;
 
 import org.eclipse.tracecompass.trace_event_logger.LogUtils.TraceEventLogRecord;
@@ -55,122 +56,168 @@ import org.eclipse.tracecompass.trace_event_logger.LogUtils.TraceEventLogRecord;
  */
 public class SnapshotHandler extends FileHandler {
 
-  // the following can be configured by Logging.properties
-  private final int maxEvents = 1000000;
-  private final double timeout = 30.0;
-  protected final String filePath = "request-"; //$NON-NLS-1$
-  // Enable scope logs by default
-  private volatile boolean isEnabled = true;
+	// the following can be configured by Logging.properties
+	private int maxEvents = 1000000;
+	private double timeout = 30.0;
+	protected String filePath = "request-"; //$NON-NLS-1$
+	// Enable scope logs by default
+	private volatile boolean isEnabled = true;
 
-  private Deque<InnerEvent> fData = new ArrayDeque<>();
-  private Map<String, Map<String, List<InnerEvent>>> fStacks = new HashMap<>();
+	private Deque<InnerEvent> fData = new ArrayDeque<>();
+	private Map<String, Map<String, List<InnerEvent>>> fStacks = new HashMap<>();
 
-  /**
-   * Snapshot handler constructor
-   *
-   * @throws IOException
-   *             If there is a file error
-   * @throws SecurityException
-   *             if we don't have the permissions
-   */
-  public SnapshotHandler() throws IOException, SecurityException {
-    super();
-  }
+	/**
+	 * Snapshot handler constructor
+	 *
+	 * @throws IOException       If there is a file error
+	 * @throws SecurityException if we don't have the permissions
+	 */
+	public SnapshotHandler() throws IOException, SecurityException {
+		super();
+		configure();
+	}
 
-  @Override
-  public boolean isLoggable(LogRecord record) {
-    return (isEnabled && record != null
-            && super.isLoggable(record)
-            && (record.getLevel().intValue() <= Level.FINE.intValue())
-            && (record instanceof TraceEventLogRecord)); // add feature switch here
-  }
+	/**
+	 * Snapshot handler constructor
+	 * 
+	 * @param timeout the timeout in seconds
+	 *
+	 * @throws IOException       If there is a file error
+	 * @throws SecurityException if we don't have the permissions
+	 */
+	public SnapshotHandler(double timeout) throws IOException, SecurityException {
+		super();
+		configure();
+		if (timeout > 0.0) {
+			this.timeout = timeout;
+		}
+	}
 
+	private void configure() {
+		LogManager manager = LogManager.getLogManager();
 
+		String cname = getClass().getName();
+		String prop = manager.getProperty(cname + ".maxEvents");
+		maxEvents = 1000000;
+		try {
+			maxEvents = Integer.parseInt(prop.trim());
+		} catch (Exception ex) {
+			// we tried!
+		}
+		if (maxEvents < 0) {
+			maxEvents = 1000000;
+		}
+		timeout = 10000;
+		prop = manager.getProperty(cname + ".timeout");
+		try {
+			timeout = Double.parseDouble(prop.trim());
+		} catch (Exception ex) {
+			// we tried!
+		}
+		if (timeout < 0) {
+			timeout = 30.0;
+		}
+		filePath = "request-";
+		prop = manager.getProperty(cname + ".filePath");
+		try {
+			filePath = prop.trim();
+		} catch (Exception ex) {
+			// we tried!
+		}
+	}
 
-  private boolean addToSnapshot(LogRecord message) {
-    InnerEvent event = InnerEvent.create(message);
-    if (event == null) {
-      return false;
-    }
-    fData.add(event);
-    while (fData.size() > maxEvents) {
-      fData.remove();
-    }
-    Map<String, List<InnerEvent>> pidMap =
-        fStacks.computeIfAbsent(event.getPid(), unused -> new HashMap<>());
-    List<InnerEvent> stack = pidMap.computeIfAbsent(event.getTid(), unused -> new ArrayList<>());
-    String phase = event.getPhase();
-    switch (phase) {
-      case "B": //$NON-NLS-1$
-        {
-          stack.add(event);
-          break;
-        }
-      case "E": //$NON-NLS-1$
-        {
-          InnerEvent lastEvent = stack.remove(stack.size() - 1);
-          if (stack.isEmpty()) {
-            double delta = event.getTs() - lastEvent.getTs();
-            if (delta > timeout) {
-              drain(fData);
-            }
-          }
-          break;
-        }
-      default:
-        // do nothing
-    }
-    return true;
-  }
+	@Override
+	public boolean isLoggable(LogRecord record) {
+		return (isEnabled && record != null && super.isLoggable(record)
+				&& (record.getLevel().intValue() <= Level.FINE.intValue()) && (record instanceof TraceEventLogRecord)); // add
+																														// feature
+																														// switch
+																														// here
+	}
 
-  @Override
-    public synchronized void publish(LogRecord record) {
-        if(record != null) {
-            addToSnapshot(record);
-        }
-        super.publish(record);
-    }
+	private boolean addToSnapshot(LogRecord message) {
+		InnerEvent event = InnerEvent.create(message);
+		if (event == null) {
+			return false;
+		}
+		fData.add(event);
+		while (fData.size() > maxEvents) {
+			fData.remove();
+		}
+		Map<String, List<InnerEvent>> pidMap = fStacks.computeIfAbsent(event.getPid(), unused -> new HashMap<>());
+		List<InnerEvent> stack = pidMap.computeIfAbsent(event.getTid(), unused -> new ArrayList<>());
+		String phase = event.getPhase();
+		switch (phase) {
+		case "B": //$NON-NLS-1$
+		{
+			stack.add(event);
+			break;
+		}
+		case "E": //$NON-NLS-1$
+		{
+			InnerEvent lastEvent = stack.remove(stack.size() - 1);
+			if (stack.isEmpty()) {
+				double delta = (event.getTs() - lastEvent.getTs()) * 0.000001; // convert to seconds
+				if (delta > timeout) {
+					drain(fData);
+				}
+			}
+			break;
+		}
+		default:
+			// do nothing
+		}
+		return true;
+	}
 
-  private void drain(Deque<InnerEvent> data) {
-    Thread thread =
-        new Thread(
-            () -> {
-              Path path = new File(filePath + data.getFirst().getTs() + ".json").toPath(); //$NON-NLS-1$
-              try (BufferedWriter fw = Files.newBufferedWriter(path, Charset.defaultCharset())) {
-                fw.write('[');
-                boolean first = true;
-                for (InnerEvent event : data) {
-                  if (first) {
-                    first = false;
-                  } else {
-                    fw.write(',');
-                    fw.write('\n');
-                  }
-                  fw.write(event.getMessage());
-                }
-                data.clear();
-                fw.write(']');
-              } catch (IOException e) {
-                // we tried!
-              }
-            });
-    thread.setName("Trace Drainer"); //$NON-NLS-1$
-    thread.start();
-  }
+	@Override
+	public synchronized void publish(LogRecord record) {
+		if (record != null) {
+			addToSnapshot(record);
+		}
+		super.publish(record);
+	}
 
-  /**
-   * Enable or disable snapshotter
-   * @param isEnabled true is enabled, false is disabled
-   */
-  public void setEnabled(Boolean isEnabled) {
-    this.isEnabled = isEnabled;
-  }
+	private void drain(Deque<InnerEvent> data) {
+		Thread thread = new Thread(() -> {
+			Path path = new File(filePath + Long.toString((long) data.getFirst().getTs()) + ".json").toPath(); //$NON-NLS-1$
+			try (BufferedWriter fw = Files.newBufferedWriter(path, Charset.defaultCharset())) {
+				fw.write('[');
+				boolean first = true;
+				for (InnerEvent event : data) {
+					if (first) {
+						first = false;
+					} else {
+						fw.write(',');
+						fw.write('\n');
+					}
+					fw.write(event.getMessage());
+				}
+				data.clear();
+				fw.write(']');
+			} catch (IOException e) {
+				// we tried!
+			}
+		});
+		thread.setName("Trace Drainer"); //$NON-NLS-1$
+		thread.start();
+	}
 
-  /**
-   * Is the snapshotter enabled?
-   * @return true if enabled
-   */
-  public boolean isEnabled() {
-    return isEnabled;
-  }
+	/**
+	 * Enable or disable snapshotter
+	 * 
+	 * @param isEnabled true is enabled, false is disabled
+	 */
+	public void setEnabled(Boolean isEnabled) {
+		this.isEnabled = isEnabled;
+	}
+
+	/**
+	 * Is the snapshotter enabled?
+	 * 
+	 * @return true if enabled
+	 */
+	public boolean isEnabled() {
+		return isEnabled;
+	}
 }
