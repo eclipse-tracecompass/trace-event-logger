5a11fe6f5d07048cd58f2c5363de9f4ed2ebd078
Matthew Khouzam <matthew.khouzam@ericsson.com>
2024-01-26 19:56:03 -0500

    Update Benchmark
    Use logging properties to parametrize asynclogger. Also fix header.
    
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

--- src/test/java/org/eclipse/tracecompass/trace_event_logger/TestLoggerBenchmark.java
+++ src/test/java/org/eclipse/tracecompass/trace_event_logger/TestLoggerBenchmark.java
@@ -1,9 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Ericsson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * SPDX-License-Identifier: MIT
+ *******************************************************************************/
+
 package org.eclipse.tracecompass.trace_event_logger;
 
 import static org.junit.Assert.fail;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,6 +39,7 @@ import java.util.logging.FileHandler;
 import java.util.logging.Formatter;
 import java.util.logging.Handler;
 import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
@@ -26,11 +53,12 @@ public class TestLoggerBenchmark {
 	private Handler oldFileHandler;
 	private Handler newFileHandler;
 	private File[] files = new File[2];
+	private long warmUp = 2000;
+	private long maxRuns = warmUp * 100;
+	private double growth = 2.3;
 
 	@Test
 	public void testBench() throws SecurityException, IOException {
-		long warmUp = 2000;
-		long maxRuns = warmUp * 10;
 		fLogger = Logger.getAnonymousLogger();
 		files[0] = File.createTempFile("trace-old" ,".json");
 		files[1] = File.createTempFile("trace-new" ,".json");
@@ -50,7 +78,7 @@ public class TestLoggerBenchmark {
 		List<Long> syncNew = new ArrayList<>();
 		List<Long> syncOld = new ArrayList<>();
 		List<Long> run = new ArrayList<>();
-		for (long runs = 2000; runs < maxRuns; runs *= 1.4) {
+		for (long runs = warmUp; runs < maxRuns; runs *= growth) {
 			for (long i = 0; i < warmUp; i++) {
 				try (LogUtils.ScopeLog log = new LogUtils.ScopeLog(logger, Level.FINE, "foo")) {
 					try (LogUtils.ScopeLog log1 = new LogUtils.ScopeLog(logger, Level.FINER, "bar")) {
@@ -110,16 +138,9 @@ public class TestLoggerBenchmark {
 			run.add(runs);
 		}
 		fLogger.removeHandler(oldFileHandler);
-		newFileHandler = new AsyncFileHandler(files[1].getAbsolutePath());
-		newFileHandler.setFormatter(new Formatter() {
-			@Override
-			public String format(LogRecord record) {
-				return record.getMessage() + ",\n";
-			}
-		});
-		newFileHandler.setLevel(Level.ALL);
+		newFileHandler = makeAsyncFileHandler(files[1].getAbsolutePath());
 		fLogger.addHandler(newFileHandler);
-		for (long runs = 2000; runs < maxRuns; runs *= 1.4) {
+		for (long runs = warmUp; runs < maxRuns; runs *= growth) {
 			for (long i = 0; i < warmUp; i++) {
 				try (LogUtils.ScopeLog log = new LogUtils.ScopeLog(logger, Level.FINE, "foo")) {
 					try (LogUtils.ScopeLog log1 = new LogUtils.ScopeLog(logger, Level.FINER, "bar")) {
@@ -200,6 +221,7 @@ public class TestLoggerBenchmark {
 					newFileHandler.close();
 				}
 				Thread.currentThread().sleep(100);
+				
 			}
 		} catch (IOException | InterruptedException e) {
 			fail(e.toString());
@@ -208,19 +230,10 @@ public class TestLoggerBenchmark {
 
 	@Test
 	public void testLeanBench() throws SecurityException, IOException {
-		long warmUp = 2000;
-		long maxRuns = warmUp * 10;
 		fLogger = Logger.getAnonymousLogger();
 		files[0] = File.createTempFile("trace-lean-old" ,".json");
 		files[1] = File.createTempFile("trace-lean-new" ,".json");
-		oldFileHandler = new FileHandler(files[0].getAbsolutePath());
-		oldFileHandler.setFormatter(new Formatter() {
-			@Override
-			public String format(LogRecord record) {
-				return record.getMessage() + ",\n";
-			}
-		});
-		oldFileHandler.setLevel(Level.ALL);
+		oldFileHandler = makeFileHandler(files[0].getAbsolutePath());
 		fLogger.addHandler(oldFileHandler);
 		fLogger.setLevel(Level.ALL);
 		Logger logger = fLogger;
@@ -229,7 +242,7 @@ public class TestLoggerBenchmark {
 		List<Long> syncNew = new ArrayList<>();
 		List<Long> syncOld = new ArrayList<>();
 		List<Long> run = new ArrayList<>();
-		for (long runs = 2000; runs < maxRuns; runs *= 1.4) {
+		for (long runs = warmUp; runs < maxRuns; runs *= growth) {
 			for (long i = 0; i < warmUp; i++) {
 				try (LogUtils.ScopeLog log = new LogUtils.ScopeLog(logger, Level.FINE, "foo")) {
 					try (LogUtils.ScopeLog log1 = new LogUtils.ScopeLog(logger, Level.FINER, "bar")) {
@@ -281,16 +294,9 @@ public class TestLoggerBenchmark {
 			run.add(runs);
 		}
 		fLogger.removeHandler(oldFileHandler);
-		newFileHandler = new AsyncFileHandler(files[1].getAbsolutePath());
-		newFileHandler.setFormatter(new Formatter() {
-			@Override
-			public String format(LogRecord record) {
-				return record.getMessage() + ",\n";
-			}
-		});
-		newFileHandler.setLevel(Level.ALL);
+		newFileHandler = makeAsyncFileHandler(files[1].getAbsolutePath());
 		fLogger.addHandler(newFileHandler);
-		for (long runs = 2000; runs < maxRuns; runs *= 1.4) {
+		for (long runs = warmUp; runs < maxRuns; runs *= growth) {
 			for (long i = 0; i < warmUp; i++) {
 				try (LogUtils.ScopeLog log = new LogUtils.ScopeLog(logger, Level.FINE, "foo")) {
 					try (LogUtils.ScopeLog log1 = new LogUtils.ScopeLog(logger, Level.FINER, "bar")) {
@@ -347,5 +353,34 @@ public class TestLoggerBenchmark {
 			System.out.println(String.format("%d,%d,%d,%d,%d", run.get(i), syncOld.get(i), syncNew.get(i),
 					asyncOld.get(i), asyncNew.get(i)));
 		}
+	}
+	
+	private Handler makeAsyncFileHandler(String path) throws IOException {
+		try (InputStream fis = new FileInputStream(new File(
+				"./src/test/java/org/eclipse/tracecompass/trace_event_logger/res/benchmarklogging.properties"))) {
+			LogManager manager = LogManager.getLogManager();
+			manager.readConfiguration(fis);
+		}
+		Handler handler = new AsyncFileHandler(path);
+		handler.setFormatter(new Formatter() {
+			@Override
+			public String format(LogRecord record) {
+				return record.getMessage() + ",\n";
+			}
+		});
+		handler.setLevel(Level.ALL);
+		return handler;
+	}
+
+	private Handler makeFileHandler(String path) throws IOException {
+		FileHandler handler = new FileHandler(path);
+		handler.setFormatter(new Formatter() {
+			@Override
+			public String format(LogRecord record) {
+				return record.getMessage() + ",\n";
+			}
+		});
+		handler.setLevel(Level.ALL);
+		return handler;
 	}
 }
